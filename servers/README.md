1979ad7065f688093d225098404ae353295788fe
olivier <olivier.winter@hotmail.fr>
2025-03-21 09:42:47 +0000

    prefect orchestration in containers for spike sorting, large and small jobs

--- servers/README.md
+++ servers/README.md
@@ -2,9 +2,71 @@
 
 The playbook are targeted for Ubuntu family machines: Ubuntu and Mint. They may work or not on Debian OS.
 
+## Install Ansible
+
+```shell
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+``` 
+
+Clone the IBL sre repository 
+```shell
+mkdir -p ~/Documents/PYTHON
+cd ~/Documents/PYTHON
+git clone git@github.com:int-brain-lab/iblsre.git
+```
+
+
+Pre-requisistes
+TODO: install prefect and configure concurrency as an ansible workflow
+TODO: recover docker logs in prefect 
+TODO: procedure for canary and update
+TODO: add containers for each env: ibllib, iblsorter, suite2p, litpose, dlc
+
+```shell
+mkdir /mnt/s0/logs
+# TODO install python with uv
+uv pip install prefect
+prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api
+prefect server start
+prefect work-pool create --type docker iblserver-docker-pool
+prefect worker start --pool iblserver-docker-pool
+# TODO install prefect and configure concurrency command line - provide a yaml file
+# TODO setup prefect as a service: https://docs.prefect.io/v3/deploy/daemonize-processes
+
+cd ~/Documents/PYTHON/iblsre/servers/containers
+python iblserver_prefect.py   # NB: this needs to be run here: the relative path of this script needs to be the same as the relative path
+prefect deployment run 'create-jobs/iblserver-create-jobs'
+```
+
+
 ## Install Docker with Support for Nvidia
 
 ```shell
+cd ~/Documents/PYTHON/iblsre/servers/ansible
 ansible-playbook docker-nvidia-container-setup.yaml --ask-become-pass
+```
+
+
+## Build containers
+The containers are setup with 2 images: a base image that contains all of the heavy lifting installation, and a second image that contains a few copy and git pull instructions to update the containers.
+The idea is to be able to re-build and manipulate often only the small top layers, while the heavy layers are more stable.
+
+```shell
+DOCKER_BUILD_PATH=~/Documents/PYTHON/iblsre/servers/containers
+docker buildx build $DOCKER_BUILD_PATH --platform linux/amd64 --tag internationalbrainlab/ibllib:latest -f $DOCKER_BUILD_PATH/Dockerfile_ibllib_base
+docker buildx build $DOCKER_BUILD_PATH --platform linux/amd64 --tag internationalbrainlab/ibllib:latest -f $DOCKER_BUILD_PATH/Dockerfile_ibllib --no-cache
+docker buildx build $DOCKER_BUILD_PATH --platform linux/amd64 --tag internationalbrainlab/iblsorter_base:latest  -f $DOCKER_BUILD_PATH/Dockerfile_iblsorter_base
+docker buildx build $DOCKER_BUILD_PATH --platform linux/amd64 --tag internationalbrainlab/iblsorter:latest  -f $DOCKER_BUILD_PATH/Dockerfile_iblsorter --no-cache
+cd ~/Documents/PYTHON/iblsre/servers/containers
+python iblserver_prefect.py   # NB: this needs to be run in the same directory as the script: the relative path of this script needs to be the same as the relative path
+prefect deployment run iblsorter-jobs/iblserver-iblsorter-jobs
+```
+
+```shell
+docker image push internationalbrainlab/ibllib:latest
+...
 ```
 
---
+++ servers/containers/iblserver_prefect.py
@@ -0,0 +1,261 @@
+"""Pipeline entry script.
+
+This script searches for new sessions (those with a raw_session.flag file), and creates the pipeline
+preprocessing tasks in Alyx. No file registration takes place in this script.
+"""
+
+import traceback
+import datetime
+import logging
+from pathlib import Path
+
+from prefect import flow, task, deploy, concurrency
+
+from one.api import ONE
+from one.webclient import AlyxClient
+from one.remote.globus import get_local_endpoint_id
+
+from ibllib.pipes.local_server import task_queue, report_health, job_creator
+from ibllib.pipes.tasks import run_alyx_task
+from ibllib.pipes.base_tasks import Task
+
+MAX_TASKS = 10
+_logger = logging.getLogger('ibllib')
+subjects_path = Path('/mnt/s0/Data/Subjects/')
+
+class JobCreator(Task):
+    """A task for creating session preprocessing tasks."""
+    level = 0
+    priority = 100
+    io_charge = 20
+
+    def __init__(self, subjects_path, **kwargs):
+        """A task for creating session preprocessing tasks.
+
+        Parameters
+        ----------
+        subjects_path : pathlib.Path
+            The root path containing the sessions to register.
+        """
+        self.subjects_path = subjects_path
+        self.pipes = []
+        super().__init__(None, **kwargs)
+
+    def _run(self):
+        # Label the lab endpoint json field with health indicators
+        try:
+            report_health(self.one.alyx)
+            _logger.info('Reported health of local server')
+        except BaseException:
+            _logger.error(f'Error in report_health\n {traceback.format_exc()}')
+
+        #  Create sessions: for this server, finds the extract_me flags, identify the session type,
+        #  create the session on Alyx if it doesn't already exist, register the raw data and create
+        #  the tasks backlog
+        pipes, _ = job_creator(self.subjects_path, one=self.one, dry=False)
+        self.pipes.extend(pipes)
+        _logger.info('Ran job creator.')
+
+
+def get_repo_from_endpoint_id(endpoint=None, alyx=None):
+    """
+    Extracts data repository name associated with a given a Globus endpoint UUID.
+
+    Parameters
+    ----------
+    endpoint : uuid.UUID, str
+        Endpoint UUID, optional if not given will get attempt to find local endpoint UUID.
+    alyx : one.webclient.AlyxClient
+        An instance of AlyxClient to use.
+
+    Returns
+    -------
+    str
+        The data repository name associated with the endpoint UUID.
+    """
+    alyx = alyx or AlyxClient(silent=True)
+    if not endpoint:
+        endpoint = get_local_endpoint_id()
+    repo = alyx.rest('data-repository', 'list', globus_endpoint_id=endpoint)
+    if any(repo):
+        return repo[0]['name']
+
+
+def run_job_creator_task(one=None, data_repository_name=None, root_path=subjects_path):
+    """Run the JobCreator task.
+
+    Parameters
+    ----------
+    one : one.api.OneAlyx
+        An instance of ONE to use.
+    data_repository_name : str
+        The associated data repository name. If None, this is determined from the local Globus
+        endpoint ID.
+    root_path : pathlib.Path
+        The root path containing the sessions to register.
+
+    Returns
+    -------
+    JobCreator
+        The run JobCreator task.
+    """
+    one = one or ONE(cache_rest=None, mode='remote')
+    data_repository_name = data_repository_name or get_repo_from_endpoint_id(alyx=one.alyx)
+    tasks = one.alyx.rest(
+        'tasks', 'list', name='JobCreator', django=f'data_repository__name,{data_repository_name}', no_cache=True)
+    assert len(tasks) < 2
+    if not any(tasks):
+        t = JobCreator(root_path, one=one, clobber=True)
+        task_dict = {
+            'executable': 'deploy.serverpc.crontab.report_create_jobs.JobCreator',
+            'priority': t.priority, 'io_charge': t.io_charge, 'gpu': t.gpu, 'cpu': t.cpu,
+            'ram': t.ram, 'module': 'deploy.serverpc.crontab.report_create_jobs',
+            'parents': [], 'level': t.level, 'status': 'Empty', 'name': t.name, 'session': None,
+            'graph': 'Base', 'arguments': {}, 'data_repository': data_repository_name}
+        talyx = one.alyx.rest('tasks', 'create', data=task_dict)
+    else:
+        talyx = tasks[0]
+        tkwargs = talyx.get('arguments') or {}  # if the db field is null it returns None
+        t = JobCreator(root_path, one=one, taskid=talyx['id'], clobber=True, **tkwargs)
+
+    one.alyx.rest('tasks', 'partial_update', id=talyx['id'], data={'status': 'Started'})
+    status = t.run()
+    patch_data = {
+        'time_elapsed_secs': t.time_elapsed_secs, 'log': t.log, 'version': t.version,
+        'status': 'Empty' if status == 0 else 'Errored'}
+    t = one.alyx.rest('tasks', 'partial_update', id=talyx['id'], data=patch_data)
+    return t
+
+
+def _get_jobs(mode, env=(None,), max_tasks=MAX_TASKS):
+    one = ONE(cache_rest=None)
+    waiting_tasks = task_queue(mode=mode, lab=None, alyx=one.alyx, env=env)
+    
+    futures = []
+    last_session = None
+    c = 0
+    for tdict in waiting_tasks:
+        if c >= max_tasks:
+            break
+        # In the case of small tasks we run a set of them at a time before re-querying
+        # Often they are from the same session, so we cache the session path between tasks
+        if last_session != tdict['session']:
+            ses = one.alyx.rest('sessions', 'list', django=f"pk,{tdict['session']}")[0]
+            session_path = Path(subjects_path).joinpath(
+                ses['subject'], ses['start_time'][:10], str(ses['number']).zfill(3))
+            last_session = tdict['session']
+        # Here we also want to make sure the task has no missing dependency
+        if len(tdict['parents']):
+            job_deck = one.alyx.rest('tasks', 'list', session=tdict['session'], no_cache=True)
+            parent_tasks = filter(lambda x: x['id'] in tdict['parents'], job_deck)
+            parent_statuses = [j['status'] for j in parent_tasks]
+            # if any of the parent tasks is not complete, throw a warning
+            if not set(parent_statuses) <= {'Complete', 'Incomplete'}:
+                _logger.warning(f"{tdict['name']} has unmet dependencies")
+                continue
+        # It is useful to label each task as best we can to handle concurrency at a global level
+        extra_tags = []
+        if tdict['gpu'] > 0:
+            extra_tags.append('gpu')
+        job_name = f"{session_path.relative_to(subjects_path)} {tdict['name']}"
+        _logger.info(f"Submitting task {job_name}")
+        dynamic_task = task(
+            name=job_name,
+            tags=[f"{mode}_jobs"] + extra_tags,
+            )(run_alyx_task)
+        futures.append(dynamic_task.submit(tdict=tdict, session_path=session_path, one=one, mode='raise'))
+        c += 1
+    return futures
+
+
+def list_available_envs(root=Path.home() / 'Documents/PYTHON/envs'):
+    """
+    List all the envs within `root` dir.
+
+    Parameters
+    ----------
+    root : str, pathlib.Path
+        The directory containing venvs.
+
+    Returns
+    -------
+    list of str
+        A list of envs, including None (assumed to be base iblenv).
+    """
+    try:
+        envs = filter(Path.is_dir, Path(root).iterdir())
+        return [None, *sorted(x.name for x in envs)]
+    except FileNotFoundError:
+        return [None]
+
+
+@flow(log_prints=True)
+def small_jobs():
+    for future in _get_jobs(mode='small', max_tasks=120):
+        future.wait()
+
+@flow(log_prints=True)
+def large_jobs():
+    for future in _get_jobs(mode='large'):
+        future.wait()
+
+@flow(log_prints=True)
+def iblsorter_jobs():
+    for future in _get_jobs(mode='all', env=['iblsorter'], max_tasks=1):
+        future.wait()
+
+
+@flow(log_prints=True)
+def create_jobs():
+    print('starting job creation task')
+    run_job_creator_task()
+
+
+if __name__ == "__main__":
+    kwargs_job_variables = dict(
+        volumes=[
+            '/mnt/s0:/mnt/s0',
+            '/home/ibladmin/.one:/root/.one',
+            '/home/ibladmin/.globusonline:/root/.globusonline',
+            '/mnt/h1:/scratch',  # TODO this is a parameter
+            ],
+        user='ibladmin',
+        image_pull_policy='Never',
+        )
+    kwargs_deploy = dict(
+        work_pool_name="iblserver-docker-pool",
+        push=False,
+        build=False,
+    )
+    deploy(
+        create_jobs.to_deployment(
+            name="iblserver-create-jobs",
+            job_variables=kwargs_job_variables,
+            concurrency_limit=1,
+            interval=datetime.timedelta(minutes=20)
+            ),
+        small_jobs.to_deployment(
+            name='iblserver-small-jobs',
+            job_variables=kwargs_job_variables,
+            concurrency_limit=3,
+            interval=datetime.timedelta(minutes=120)
+            ),
+        large_jobs.to_deployment(
+            name='iblserver-large-jobs',
+            job_variables=kwargs_job_variables,
+            concurrency_limit=1,
+            interval=datetime.timedelta(minutes=360)
+            ),
+        image="internationalbrainlab/ibllib:latest",
+        **kwargs_deploy
+    )
+    deploy(
+        iblsorter_jobs.to_deployment(
+            name='iblserver-iblsorter-jobs',
+            job_variables=kwargs_job_variables,
+            concurrency_limit=1,
+            interval=datetime.timedelta(minutes=60 * 3)
+            ),
+        image="internationalbrainlab/iblsorter:latest",
+        **kwargs_deploy
+        )
