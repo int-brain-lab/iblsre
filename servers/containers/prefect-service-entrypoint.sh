0ea99547c79ec33d5d8fbd79fb16fa9119fa007c
ibladmin <ibladmin@iblserver.(none)>
2025-04-23 13:04:27 -0700

    first working version

--- servers/containers/prefect-service-entrypoint.sh
+++ servers/containers/prefect-service-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-RUN prefect work-pool create --type docker iblserver-docker-pool
-RUN prefect worker start --pool iblserver-docker-pool
+prefect work-pool create --type docker iblserver-docker-pool
+
 # NB: this needs to be run here: the relative path of this script needs to be the same as the relative path in the worker container
-RUN python iblserver_prefect.py
+python iblserver_prefect.py
+
+prefect worker start --pool iblserver-docker-pool
+
