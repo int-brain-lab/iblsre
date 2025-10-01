238f3e465ae622f6902bf8b182fde0d007505449
Olivier Winter <olivier.winter@hotmail.fr>
2025-10-01 21:04:41 +0100

    force certificate renewal

--- alyx/hosts/alyx-test/02_renew_certificate.sh
+++ alyx/hosts/alyx-test/02_renew_certificate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker exec -it alyx_apache certbot --apache --noninteractive --agree-tos --email admin@internationalbrainlab.org -d test.alyx.internationalbrainlab.org
+docker exec -it alyx_apache certbot --apache --noninteractive --agree-tos --email admin@internationalbrainlab.org -d test.alyx.internationalbrainlab.org --force-renewal
