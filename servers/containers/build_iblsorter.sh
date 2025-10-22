0c5e9651d1bd38c0cedb342bcc7bce9555da1bfa
Olivier <olivier.winter@hotmail.fr>
2025-10-22 18:38:06 +0100

    multi-stage containers for multiple CUDA versions

---
+++ servers/containers/build_iblsorter.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+DOCKER_BUILD_PATH=~/Documents/PYTHON/iblsre/servers/containers
+CUDA_MAJOR_VERSION=11
+IBLSORTER_VERSION=1.12
+IBLLIB_BRANCH=prefect
+SCRATCH_DIR=/home/$USER/scratch
+
+# build the base version of the image (this is quite long)
+docker buildx build $DOCKER_BUILD_PATH \
+  --platform linux/amd64 \
+  --tag internationalbrainlab/iblsorter_base:cuda${CUDA_MAJOR_VERSION} \
+  --build-arg CUDA_MAJOR_VERSION=${CUDA_MAJOR_VERSION} \
+  -f $DOCKER_BUILD_PATH/Dockerfile_iblsorter_base
+
+# build the head version of the container
+docker buildx build $DOCKER_BUILD_PATH \
+	--platform linux/amd64 \
+	--tag internationalbrainlab/iblsorter_cuda${CUDA_MAJOR_VERSION}:${IBLSORTER_VERSION} \
+  -f $DOCKER_BUILD_PATH/Dockerfile_iblsorter \
+  --no-cache \
+  --build-arg ibllib_branch=${IBLLIB_BRANCH}
+
+docker run \
+  -it \
+  --rm \
+  --name spikesorter \
+  -v /mnt/s0:/mnt/s0 \
+  -v /home/$USER/.one:/home/ibladmin/.one \
+  -v ${SCRATCH_DIR}:/scratch internationalbrainlab/iblsorter_cuda${CUDA_MAJOR_VERSION}:${IBLSORTER_VERSION}
+  /bin/bash iblsorter-test
